567f00df40f2b8a5818e0ebc0c7a8b2f567311fe
Myakovlev97 <myakovlev97@yandex.ru>
2025-01-12 19:14:39 +0200

    Adding project files

--- Misha/Lesson_7/CW_7.7.py
+++ Misha/Lesson_7/CW_7.7.py
@@ -1,5 +1,4 @@
-a = 12231
-tuple_example = [a, 22, 2], False, "a", [12, "qw", a], (a, "bc", [2, 1, a, "bro"])
+tuple_example = 11, [22, 33], 44, 55, 66
 tuple_example = list(tuple_example)
-tuple_example[0][1] = 222
+tuple_example[1][0] = 222
 print(tuple_example)
